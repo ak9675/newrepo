5f92865da5a8cbd742d99e6fcc17d4f7ee7149b0
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-01-21 06:59:52 +0000

    Update newrg.tf

--- newrg.tf
+++ newrg.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "example" {
   name     = "day4"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example1" {
   name     = "day5"
   location = "West Europe"
 }
